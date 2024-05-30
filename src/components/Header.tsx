b816a44eb2b1066cbdcca7af3abf35922388a900
Osamah Ahmad <osamah_b@outlook.com>
2024-05-30 22:23:39 +0000

    fix dependencies and remove auth anim on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React, { FC, ReactNode } from 'react';
 import './Header.css';
 import Logo from './Logo.tsx';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 interface HeaderProps {
     nav?: ReactNode;
@@ -9,7 +9,6 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ nav }) => {
     const location = useLocation();
-    const navigate = useNavigate();
 
     return <header>
         <Logo disabled={location.pathname === '/'} />
